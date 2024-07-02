769d893d3cde6320b881131bc41f37713efa6cc3
ARUNKUMAR660 <kumarparun660@gmail.com>
2024-07-02 22:48:26 +0530

    initial

---
+++ index.py
@@ -0,0 +1,26 @@
+class Song:
+    
+    def __init__(self,name,artist,album,length):
+        self.name=name
+        self.artist=artist
+        self.album=album
+        self.length=length
+        
+    def get_name(self):
+        return self.name
+    def set_name(self,name):
+        self.name=name
+    def get_artist(self):
+        return self.artist
+    def set_name(self,artist):
+        self.artist=artist
+    def get_album(self):
+        return self.album
+    def set_album(self,album):
+        self.album=album
+    def get_length(self):
+        return self.length
+    def set_length(self,length):
+        self.length=length
+        
+        
